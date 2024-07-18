cfe4f599fabbceaa3293e4fc4f1bd6a194f22dd2
Alr-Dev <157879926+Alr-Dev@users.noreply.github.com>
2024-07-18 19:56:23 -0300

    Update Loadstring2.lua

--- LoadString/Loadstring2.lua
+++ LoadString/Loadstring2.lua
@@ -1,1 +1,14 @@
+-- Variáveis globais
+local Players = game:GetService("Players")
+local TweenService = game:GetService("TweenService")
+local player = Players.LocalPlayer
+local waypoints = {}
+local currentRouteIndex = 1
+local currentWaypointIndex = 1
+local isIAActive = false
+local humanoid = nil
+local tween = nil
+local buttonGui = nil
 
+-- Tempo de movimento entre waypoints (em segundos)
+local moveTime = 1  -- Altere conforme necessário
