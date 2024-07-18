ab98be9ef720a8a99735d8a0e29fef2effe7f3af
Alr-Dev <157879926+Alr-Dev@users.noreply.github.com>
2024-07-18 19:56:07 -0300

    Update main2.lua

--- main2.lua
+++ main2.lua
@@ -51,20 +51,7 @@ local function createButtonGui()
     return activateButton, deactivateButton
 end
 
--- Variáveis globais
-local Players = game:GetService("Players")
-local TweenService = game:GetService("TweenService")
-local player = Players.LocalPlayer
-local waypoints = {}
-local currentRouteIndex = 1
-local currentWaypointIndex = 1
-local isIAActive = false
-local humanoid = nil
-local tween = nil
-local buttonGui = nil
 
--- Tempo de movimento entre waypoints (em segundos)
-local moveTime = 1  -- Altere conforme necessário
 
 -- Função para mover para o próximo waypoint
 local function moveToNextWaypoint()
